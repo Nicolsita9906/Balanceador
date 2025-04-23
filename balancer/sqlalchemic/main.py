f82c8c7bdd255c11fdd83f5b11a3614930e183a1
Nicolsita9906 <nicolvaleriaocampobernal@gmail.com>
2025-04-23 08:25:21 -0500

    first commit

--- balancer/sqlalchemic/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from models import User
-from database import engine, Base
-from routes import router
-
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI()
-app.include_router(router)
